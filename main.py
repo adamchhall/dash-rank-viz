da8917167691ccae2b601ee945033cf0e94f41c7
Adam Hall <adam@marvin.fios-router.home>
2020-07-22 14:00:12 -0400

    Changed host from localhost to 0.0.0.0

--- main.py
+++ main.py
@@ -352,4 +352,4 @@ def update_heatmap(selected_rows):
 app.config['suppress_callback_exceptions'] = True
 
 #if __name__ == '__main__':
-app.run_server(port=8080, debug=True)
+app.run_server(host='0.0.0.0', port=8080, debug=True)
